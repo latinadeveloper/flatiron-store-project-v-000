aafe20f73a2d33e562f9acba72cced31118175e0
Isis Tejeda <isis@isistejeda.com>
2017-05-25 20:28:39 -0500

    cart created

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,3 +1,11 @@
 class LineItemsController < ApplicationController
+  def create
+    if !current_user.current_cart #creates a new cart
+      current_user.current_cart = Cart.create
+      current_user.save
+    end
+
+  end
+
 
 end
